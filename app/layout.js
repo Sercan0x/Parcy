1d1a57d25ed3955bc21895569049e4c1a075b944
Sercan0x <ahmet.kurnaz19239999@gmail.com>
2025-11-22 05:06:58 +0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,31 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Parcy (TEST)",
+  description: "Decentralized invoicing platform on Arc Network Testnet",
+  icons: {
+    icon: 'https://i.ibb.co/nqDXzSnC/icon.png',
+    shortcut: 'https://i.ibb.co/nqDXzSnC/icon.png',
+    apple: 'https://i.ibb.co/nqDXzSnC/icon.png',
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
